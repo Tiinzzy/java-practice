e91ef72bc43f4181e9ad698d485bf026f2fc1f73
Tina <tinzzy.cracked@yahoo.com>
2023-10-29 12:08:49 -0400

    added new methods to tv series for uploading episodes and seasons

--- netflix-model/src/main/java/org/netflix/model/tv_series/TvSeriesDao.java
+++ netflix-model/src/main/java/org/netflix/model/tv_series/TvSeriesDao.java
@@ -4,49 +4,84 @@ import org.bson.Document;
 import org.netflix.utility.Database;
 import org.netflix.utility.OidGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.mongodb.client.model.Filters.eq;
 
 public class TvSeriesDao {
-    private static final String TV_SERIES_COLLECTION = "tvshows";
+    private static final String TV_SERIES_COLLECTION = "tv_series";
     private long oid = -1;
     private String title = null;
-    private String description = null;
+    private String summary = null;
     private String startDate = null;
     private String endDate = null;
 
     public TvSeriesDao() {
     }
 
-    public TvSeriesDao(String title, String description, String startDate, String endDate) {
+    public TvSeriesDao(String title, String summary, String startDate, String endDate) {
         this.oid = OidGenerator.getNew();
         this.title = title;
-        this.description = description;
+        this.summary = summary;
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
     public TvSeriesDao(long oid) {
-        this.oid = oid;
         var db = Database.INSTANCE.getNetflixDatabase();
         Document doc = db.getCollection(TV_SERIES_COLLECTION).find(eq("oid", oid)).first();
         if (doc != null) {
+            this.oid = oid;
             this.title = doc.getString("title");
-            this.description = doc.getString("description");
+            this.summary = doc.getString("summary");
             this.startDate = doc.getString("startDate");
             this.endDate = doc.getString("endDate");
         }
     }
 
-    public void saveToTable(){
+    public void saveToTable() {
         var db = Database.INSTANCE.getNetflixDatabase();
         Document document = new Document();
         document.append("oid", this.oid);
         document.append("title", this.title);
-        document.append("description", this.description);
+        document.append("summary", this.summary);
         document.append("startDate", this.startDate);
         document.append("endDate", this.endDate);
         db.getCollection(TV_SERIES_COLLECTION).insertOne(document);
     }
+
+    // Todo: Finish this tomorrow. So that loadAll("thing") returns at least "Stranger Things" and "The Thing", ...
+    public List<TvSeriesDao> loadAll(String nameContains, int count) {
+        return null;
+    }
+
+    public List<SeasonDao> loadSeasons() {
+        return SeasonDao.loadAll(this.oid);
+    }
+
+    public void addSeason(int seasonNumber, String startDate, String endDate) {
+        SeasonDao seasonDao = new SeasonDao(seasonNumber, startDate, endDate, this.oid);
+        seasonDao.saveToTable();
+    }
+
+    public void addEpisode(int seasonNumber, String title, int runTime, String airDate) {
+        var season = getSeason(seasonNumber);
+        if (season != null) {
+            season.addEpisode(title, runTime, airDate);
+        }
+    }
+
+    public SeasonDao getSeason(int seasonNumber) {
+        var seasons = this.loadSeasons();
+        for (var s : seasons) {
+            if (s.getSeasonNumber() == seasonNumber) {
+                return s;
+            }
+        }
+        return null;
+    }
+
     public long getOid() {
         return oid;
     }
@@ -55,8 +90,8 @@ public class TvSeriesDao {
         return title;
     }
 
-    public String getDescription() {
-        return description;
+    public String getSummary() {
+        return summary;
     }
 
     public String getStartDate() {
