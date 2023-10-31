50edabefce08888e47fa1cbd6fdae169f4f05797
Tina <tinzzy.cracked@yahoo.com>
2023-10-31 08:50:20 -0400

    wrote test for all movies

--- netflix-model/src/test/java/org/netflix/model/movies/AllMovieTest.java
+++ netflix-model/src/test/java/org/netflix/model/movies/AllMovieTest.java
@@ -1,4 +1,27 @@
 package org.netflix.model.movies;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class AllMovieTest {
+
+    @Test
+    void testMoviesDao() {
+        MovieDao newMovie = new MovieDao("Heart of Stone", "08/11/2023", "8.5");
+        newMovie.saveToTable();
+        assertEquals(newMovie.getMovieTitle(), "Heart of Stone");
+        assertEquals(newMovie.getReleaseDate(), "08/11/2023");
+        assertEquals(newMovie.getRating(), "8.5");
+
+
+        MovieDao getInsertedMovie = new MovieDao(newMovie.getOid());
+        assertEquals(getInsertedMovie.getOid(), newMovie.getOid());
+
+        List<MovieDao> loadAllMovies = MovieDao.loadAll();
+        assertFalse(loadAllMovies.isEmpty());
+    }
 }
