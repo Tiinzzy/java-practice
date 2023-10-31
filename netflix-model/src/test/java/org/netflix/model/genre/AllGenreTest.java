08514e5b5d17738d6e38bc6f071106267f6bc529
Tina <tinzzy.cracked@yahoo.com>
2023-10-31 08:28:54 -0400

    wrote test for all genres

--- netflix-model/src/test/java/org/netflix/model/genre/AllGenreTest.java
+++ netflix-model/src/test/java/org/netflix/model/genre/AllGenreTest.java
@@ -1,4 +1,32 @@
 package org.netflix.model.genre;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class AllGenreTest {
+
+    @Test
+    void testGenreDao() {
+    GenreDao gn = new GenreDao(-1);
+    assertEquals(gn.getOid(), -1);
+
+    GenreDao newGenre = new GenreDao("Psyhcological");
+    newGenre.saveToTable();
+    assertEquals(newGenre.getDescription(), "Psyhcological");
+
+    GenreDao newGenreName = new GenreDao(67);
+    var aString = LocalDateTime.now().toString();
+    newGenreName.setDescription(aString);
+    newGenreName.saveToTable();
+    assertEquals(newGenreName.getDescription(), aString);
+
+    List<GenreDao> loadAllGenres = GenreDao.loadAll();
+    assertFalse(loadAllGenres.isEmpty());
+
+    }
 }
