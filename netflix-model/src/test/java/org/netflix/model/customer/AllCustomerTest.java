cabbd4d82466dfe9383c044b96a9bab9a4345ea0
Tina <tinzzy.cracked@yahoo.com>
2023-10-31 08:36:24 -0400

    wrote test for customer class

--- netflix-model/src/test/java/org/netflix/model/customer/AllCustomerTest.java
+++ netflix-model/src/test/java/org/netflix/model/customer/AllCustomerTest.java
@@ -1,4 +1,26 @@
 package org.netflix.model.customer;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class AllCustomerTest {
+    @Test
+    void testEpisodeDao() {
+    CustomerDao newCustomer = new CustomerDao("Kamran", "1239754254", "kamran@email.com");
+    newCustomer.saveToTable();
+    assertEquals(newCustomer.getName(), "Kamran");
+    assertEquals(newCustomer.getPhoneNo(), "1239754254");
+    assertEquals(newCustomer.getEmail(), "kamran@email.com");
+
+    CustomerDao searchCustomer = new CustomerDao(70);
+    assertEquals(searchCustomer.getOid(), 70);
+
+    List<CustomerDao> loadAllCustomers = CustomerDao.loadAll();
+    assertFalse(loadAllCustomers.isEmpty());
+    }
+
 }
