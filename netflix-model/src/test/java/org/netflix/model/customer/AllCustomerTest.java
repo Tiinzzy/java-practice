144682998e3777a3965779188e62aaa793bbbf83
Tina <tinzzy.cracked@yahoo.com>
2023-10-31 10:52:21 -0400

    wrote tests for customer class

--- netflix-model/src/test/java/org/netflix/model/customer/AllCustomerTest.java
+++ netflix-model/src/test/java/org/netflix/model/customer/AllCustomerTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class AllCustomerTest {
     @Test
-    void testEpisodeDao() {
+    void testCustomerDao() {
     CustomerDao newCustomer = new CustomerDao("Kamran", "1239754254", "kamran@email.com");
     newCustomer.saveToTable();
     assertEquals(newCustomer.getName(), "Kamran");
@@ -21,6 +21,39 @@ public class AllCustomerTest {
 
     List<CustomerDao> loadAllCustomers = CustomerDao.loadAll();
     assertFalse(loadAllCustomers.isEmpty());
+    }
+
+    @Test
+    void testCustomerUpdate(){
+        CustomerDao newCustomer = new CustomerDao("Tinzzy", "1239754254", "tinzzy@email.com");
+        newCustomer.saveToTable();
+        assertEquals(newCustomer.getName(), "Tinzzy");
+        assertEquals(newCustomer.getPhoneNo(), "1239754254");
+        assertEquals(newCustomer.getEmail(), "tinzzy@email.com");
+
+        CustomerDao updateCustomer = new CustomerDao(newCustomer.getOid());
+        updateCustomer.setEmail("tintin@email.com");
+        updateCustomer.setName("Tintin");
+        updateCustomer.setPhoneNo("5149871234");
+        updateCustomer.saveToTable();
+        updateCustomer.saveToTable();
+
+        assertEquals(updateCustomer.getName(), "Tintin");
+        assertEquals(updateCustomer.getPhoneNo(), "5149871234");
+        assertEquals(updateCustomer.getEmail(), "tintin@email.com");
+    }
+
+    @Test
+    void testDeleteCustomer(){
+        CustomerDao newCustomer = new CustomerDao("Test1", "123456789", "test1@email.com");
+        newCustomer.saveToTable();
+        assertEquals(newCustomer.getName(), "Test1");
+        assertEquals(newCustomer.getPhoneNo(), "123456789");
+        assertEquals(newCustomer.getEmail(), "test1@email.com");
+
+        long customerId = newCustomer.getOid();
+        newCustomer.deleteACustomer(customerId);
+        assertEquals(newCustomer.getOid(), customerId);
     }
 
 }
