19187444466483a8cfeac6064d617a86f9fbe021
Tina <tinzzy.cracked@yahoo.com>
2023-10-31 09:44:39 -0400

    ran test for update and delete

--- netflix-model/src/test/java/org/netflix/model/genre/AllGenreTest.java
+++ netflix-model/src/test/java/org/netflix/model/genre/AllGenreTest.java
@@ -29,4 +29,27 @@ public class AllGenreTest {
     assertFalse(loadAllGenres.isEmpty());
 
     }
+
+    @Test
+    void testGenreUpdate(){
+        GenreDao gn = new GenreDao(101);
+        assertEquals(gn.getDescription(), "Psyhcological");
+        assertEquals(gn.getOid(), 101);
+
+        gn.setDescription("Western");
+        gn.saveToTable();
+        assertEquals(gn.getDescription(), "Western");
+    }
+
+    @Test
+    void testDleteGenre(){
+        GenreDao gn = new GenreDao("Sci-Fi");
+        long newGnId = gn.getOid();
+        assertEquals(gn.getDescription(), "Sci-Fi");
+        assertEquals(gn.getOid(), newGnId);
+
+        gn.deleteGenre(gn.getOid());
+        assertEquals(gn.getOid(), newGnId);
+    }
+
 }
