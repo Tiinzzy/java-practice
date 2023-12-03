d8310972fdd18776299169628e96c9b1dc55387e
Tina <tinzzy.cracked@yahoo.com>
2023-12-03 17:07:36 -0500

    created code for game of life

---
+++ FirstJavaProject/src/main/java/org/tests/HardIronCage.java
@@ -0,0 +1,23 @@
+package org.tests;
+
+public class HardIronCage implements Cage {
+    @Override
+    public void putAnimalIn(Animal a) {
+
+    }
+
+    @Override
+    public void reinforce() {
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public void paint() {
+
+    }
+}
