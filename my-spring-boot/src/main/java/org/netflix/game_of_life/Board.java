b5550933c20ee2304fea67035649714f0b4906b5
Tina <tinzzy.cracked@yahoo.com>
2023-12-04 10:48:29 -0500

    added board class for game of life

---
+++ my-spring-boot/src/main/java/org/netflix/game_of_life/Board.java
@@ -0,0 +1,18 @@
+package org.netflix.game_of_life;
+
+import netscape.javascript.JSObject;
+import org.json.JSONArray;
+
+public interface Board {
+    void initialize(int count);
+
+    void setCell(int row, int col);
+
+    void display();
+
+    int getNumberOfAliveCellAround(int row, int col);
+
+    void evolve();
+
+    JSONArray toJSON();
+}
