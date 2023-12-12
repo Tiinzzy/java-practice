3f7a6c7c59106d492e0a2c7e9156b42c52e09e5b
Tina <tinzzy.cracked@yahoo.com>
2023-12-12 13:37:29 -0500

    fixed jungle class

--- FirstJavaProject/src/main/java/org/genetic_algoritm_ii/Jungle.java
+++ FirstJavaProject/src/main/java/org/genetic_algoritm_ii/Jungle.java
@@ -1,15 +1,27 @@
 package org.genetic_algoritm_ii;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Jungle implements Environment {
+    private static final int STRENGTH_THRESHOLD = 2; // Threshold for strength
+    private final Random random = new Random();
+
     @Override
     public boolean canSurvive(Person person) {
-        return false;
+        return person.strengthGene >= STRENGTH_THRESHOLD;
     }
 
     @Override
     public List<Person> generateChildren(Couple couple) {
-        return null;
+        List<Person> children = new ArrayList<>();
+        if (canSurvive(couple.p1) && canSurvive(couple.p2)) {
+            int childCount = Config.MIN_CHILDREN + random.nextInt(Config.MAX_CHILDREN - Config.MIN_CHILDREN + 1);
+            for (int i = 0; i < childCount; i++) {
+                children.add(couple.getChild());
+            }
+        }
+        return children;
     }
 }
