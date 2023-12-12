ce6b0d007938d65d795ef260f26fa010ae2edddf
Tina <tinzzy.cracked@yahoo.com>
2023-12-12 08:49:43 -0500

    added methods needed for determining environment interface

--- FirstJavaProject/src/main/java/org/genetic_algoritm_ii/Environment.java
+++ FirstJavaProject/src/main/java/org/genetic_algoritm_ii/Environment.java
@@ -1,4 +1,8 @@
 package org.genetic_algoritm_ii;
 
+import java.util.List;
+
 public interface Environment {
+    boolean canSurvive(Person person);
+    List<Person> generateChildren(Couple couple);
 }
