e08b327dbff3191be26caf44cdb164d6dc27ce72
Tina <tinzzy.cracked@yahoo.com>
2023-12-12 08:50:16 -0500

    implementing environment methods to classes

--- FirstJavaProject/src/main/java/org/genetic_algoritm_ii/City.java
+++ FirstJavaProject/src/main/java/org/genetic_algoritm_ii/City.java
@@ -1,4 +1,15 @@
 package org.genetic_algoritm_ii;
 
+import java.util.List;
+
 public class City implements Environment {
+    @Override
+    public boolean canSurvive(Person person) {
+        return false;
+    }
+
+    @Override
+    public List<Person> generateChildren(Couple couple) {
+        return null;
+    }
 }
