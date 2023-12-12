8796ac8160f522153ed1fd69e21eb3f35b9c60ab
Tina <tinzzy.cracked@yahoo.com>
2023-12-12 13:37:20 -0500

    fixed city class

--- FirstJavaProject/src/main/java/org/genetic_algoritm_ii/City.java
+++ FirstJavaProject/src/main/java/org/genetic_algoritm_ii/City.java
@@ -1,15 +1,27 @@
 package org.genetic_algoritm_ii;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class City implements Environment {
+    private static final int INTELLIGENCE_THRESHOLD = 2; // Threshold for intelligence
+    private final Random random = new Random();
+
     @Override
     public boolean canSurvive(Person person) {
-        return false;
+        return person.intelligenceGene >= INTELLIGENCE_THRESHOLD;
     }
 
     @Override
     public List<Person> generateChildren(Couple couple) {
-        return null;
+        List<Person> children = new ArrayList<>();
+        if (canSurvive(couple.p1) && canSurvive(couple.p2)) {
+            int childCount = Config.MIN_CHILDREN + random.nextInt(Config.MAX_CHILDREN - Config.MIN_CHILDREN + 1);
+            for (int i = 0; i < childCount; i++) {
+                children.add(couple.getChild());
+            }
+        }
+        return children;
     }
 }
