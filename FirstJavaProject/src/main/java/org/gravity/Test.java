556e25090f949f23f16bc57d274616403c630a5b
Tina <tinzzy.cracked@yahoo.com>
2023-12-13 14:09:01 -0500

    changed test

--- FirstJavaProject/src/main/java/org/gravity/Test.java
+++ FirstJavaProject/src/main/java/org/gravity/Test.java
@@ -5,10 +5,16 @@ import org.json.JSONObject;
 
 public class Test {
     public static void main(String[] args) {
-        Graph g = new Graph(3);
-        g.printMatrix();
+        Graph g = new Graph(6);
+        int count = 0;
 
-        JSONArray jsonMatrix = g.getMatrixAsJson();
-        System.out.println(jsonMatrix.toString());
+        g.printMatrix();
+        System.out.println("---------------------------------------------");
+        g.merge();
+
+//        g.printMatrix();
+//
+//        JSONArray jsonMatrix = g.getMatrixAsJson();
+//        System.out.println(jsonMatrix.toString());
     }
 }
