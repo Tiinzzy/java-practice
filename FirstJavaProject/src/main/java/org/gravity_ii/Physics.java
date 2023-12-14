3aa3e2020079b0142218e63c3353d4c4e0f3cc6f
Tina <tinzzy.cracked@yahoo.com>
2023-12-14 08:50:38 -0500

    fixed methods in physics class

--- FirstJavaProject/src/main/java/org/gravity_ii/Physics.java
+++ FirstJavaProject/src/main/java/org/gravity_ii/Physics.java
@@ -2,16 +2,19 @@ package org.gravity_ii;
 
 public class Physics {
 
+    private static final float G = 6.67430e-11f;
 
     static float getForce(Particle p1, Particle p2) {
-        // return (m1 * m2) / ( (x1-x2)^2 + (y1-y2)^2 )
-        return 0F;
+        float dx = p1.x - p2.x;
+        float dy = p1.y - p2.y;
+        float distanceSquared = dx * dx + dy * dy;
+        return G * (p1.m * p2.m) / distanceSquared;
     }
 
     static Particle getMergedParticle(Particle p1, Particle p2) {
         float m = p1.m + p2.m;
-        float x = (p1.x + p2.x) / 2;
-        float y = (p1.y + p2.y) / 2;
+        float x = (p1.x * p1.m + p2.x * p2.m) / m;
+        float y = (p1.y * p1.m + p2.y * p2.m) / m;
         return new Particle(x, y, m);
     }
 }
