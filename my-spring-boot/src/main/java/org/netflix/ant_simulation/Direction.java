90ccdd26685c6fe4d5e42578217b3c42c9bae99f
Tina <tinzzy.cracked@yahoo.com>
2023-12-19 18:06:07 -0500

    added a new separate class for directions

--- my-spring-boot/src/main/java/org/netflix/ant_simulation/Direction.java
+++ my-spring-boot/src/main/java/org/netflix/ant_simulation/Direction.java
@@ -1,5 +1,32 @@
 package org.netflix.ant_simulation;
 
-public enum Direction {
-    NORTH, EAST, SOUTH, WEST
+import java.util.function.Function;
+
+public class Direction {
+
+    Function<Integer, Integer> transformX;
+    Function<Integer, Integer> transformY;
+
+    Direction left;
+    Direction right;
+
+    public Direction() {
+    }
+    public Direction(Function<Integer, Integer> transformX, Function<Integer, Integer> transformY) {
+        this.transformX = transformX;
+        this.transformY = transformY;
+    }
+
+    Integer getX(Integer x) {
+        return this.transformX.apply(x);
+    }
+
+    Integer getY(Integer y) {
+        return this.transformY.apply(y);
+    }
+
+    void setNeighbours(Direction left, Direction right) {
+        this.left = left;
+        this.right = right;
+    }
 }
