66e300eaccd8218d396b4d3370b20ab1ec25db18
Tina <tinzzy.cracked@yahoo.com>
2023-12-20 11:05:03 -0500

    created a class for ant

---
+++ my-spring-boot/src/main/java/org/netflix/ant_simulation/Ant.java
@@ -0,0 +1,30 @@
+package org.netflix.ant_simulation;
+
+public class Ant {
+    private int x;
+    private int y;
+    private Direction currentDirection;
+
+    public Ant(int x, int y, Direction startDirection) {
+        this.x = x;
+        this.y = y;
+        this.currentDirection = startDirection;
+    }
+
+    public void moveForward(Torus grid) {
+        y = grid.wrapCoordinate(currentDirection.getY(y));
+        x = grid.wrapCoordinate(currentDirection.getX(x));
+    }
+
+    public void flipDirection(boolean color) {
+        currentDirection = color ? currentDirection.right : currentDirection.left;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
