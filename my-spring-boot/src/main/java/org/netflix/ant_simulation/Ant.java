18320e5416ede93521ed1216a2a8a23dbf84f618
Tina <tinzzy.cracked@yahoo.com>
2023-12-21 10:38:46 -0500

    updated ant class to add color for each ant

--- my-spring-boot/src/main/java/org/netflix/ant_simulation/Ant.java
+++ my-spring-boot/src/main/java/org/netflix/ant_simulation/Ant.java
@@ -5,12 +5,14 @@ public class Ant {
     private int y;
     private Direction currentDirection;
     private final Torus grid;
+    private String color;
 
-    public Ant(int x, int y, Direction startDirection, Torus grid) {
+    public Ant(int x, int y, Direction startDirection, Torus grid, String color) {
         this.x = x;
         this.y = y;
         this.currentDirection = startDirection;
         this.grid = grid;
+        this.color = color;
     }
 
     public void flipDirection() {
@@ -26,6 +28,10 @@ public class Ant {
         grid.flipColor(x, y);
         y = grid.wrapCoordinate(currentDirection.transformY(y));
         x = grid.wrapCoordinate(currentDirection.transformX(x));
+    }
+
+    public String getColor() {
+        return this.color;
     }
 
 }
