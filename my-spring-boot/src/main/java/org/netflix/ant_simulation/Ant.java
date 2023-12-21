6d3e1458343c2773a3398586636f70ef91d788f9
Tina <tinzzy.cracked@yahoo.com>
2023-12-21 18:07:18 -0500

    setting color for ants based on id

--- my-spring-boot/src/main/java/org/netflix/ant_simulation/Ant.java
+++ my-spring-boot/src/main/java/org/netflix/ant_simulation/Ant.java
@@ -5,33 +5,29 @@ public class Ant {
     int y;
     private Direction currentDirection;
     private final Torus grid;
-    private String color;
+    private final int id;
 
-    public Ant(int x, int y, Direction startDirection, Torus grid, String color) {
+    public Ant(int x, int y, Direction startDirection, Torus grid, int id) {
         this.x = x;
         this.y = y;
         this.currentDirection = startDirection;
         this.grid = grid;
-        this.color = color;
+        this.id = id;
     }
 
     public void flipDirection() {
-        grid.flipColor(x, y);
+        grid.flipColor(x, y, id);
     }
 
     public void move() {
-        if (grid.getColor(x, y)) {
+        if (grid.getColor(x, y) != 0) {
             currentDirection = currentDirection.right;
         } else {
             currentDirection = currentDirection.left;
         }
-        grid.flipColor(x, y);
+        grid.flipColor(x, y, id);
         y = grid.wrapCoordinate(currentDirection.transformY(y));
         x = grid.wrapCoordinate(currentDirection.transformX(x));
-    }
-
-    public String getColor() {
-        return this.color;
     }
 
 }
