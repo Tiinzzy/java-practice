8688ddb36eff502d7356cdf2df94354590b4365e
Tina <tinzzy.cracked@yahoo.com>
2023-12-21 18:06:51 -0500

    changed grid to int from Boolean

--- my-spring-boot/src/main/java/org/netflix/ant_simulation/Torus.java
+++ my-spring-boot/src/main/java/org/netflix/ant_simulation/Torus.java
@@ -1,19 +1,23 @@
 package org.netflix.ant_simulation;
 
 public class Torus {
-    private final boolean[][] grid;
+    private final int[][] grid;
     private final int size;
 
     public Torus(int size) {
         this.size = size;
-        this.grid = new boolean[size][size];
+        this.grid = new int[size][size];
     }
 
-    public void flipColor(int x, int y) {
-        this.grid[x][y] = !this.grid[x][y];
+    public void flipColor(int x, int y, int id) {
+        if (this.grid[x][y] == 0) {
+            this.grid[x][y] = id;
+        } else {
+            this.grid[x][y] = 0;
+        }
     }
 
-    public boolean getColor(int x, int y) {
+    public int getColor(int x, int y) {
         return this.grid[x][y];
     }
 
